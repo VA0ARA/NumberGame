5bb3094194148c491b04927a2be02d526030ac5c
vahid ara <vahidaara@gmail.com>
2023-05-30 08:19:33 +0430

    chang SetFile to update it

--- Program.cs
+++ Program.cs
@@ -4,99 +4,34 @@ namespace TicTacToeGame
 {
     class Program
     {
-        public static string Player1 = "X";
-        public static string Player2 = "O";
-        public static int counter = 0;
-        public static string[,] matrics = {
-        { "1","2","3"},
-        {"4","5","6" },
-        {"7","8","9" }
-        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("***WellCome****");
-            Display();
-
-        }
-
-        public static void Display()
-        {
-            //create table
-            for (int i = 0; i < matrics.GetLength(0); i++)
-            {
-
-                for (int j = 0; j < matrics.GetLength(0); j++)
-                {
-                    Console.Write(" " + matrics[i, j] + " " + "|");
-                }
-                Console.WriteLine("");
-                Console.WriteLine("------------");
-
-            }
-        }
-        public static void ChangeContent(string Content,string player )
-        {
-    
-
-            if (Content == matrics[0,0] )
-            {
-                matrics[0,0]= player;
-            }
-            else if (Content == matrics[0, 1])
-            {
-                matrics[0, 1] = player;
-            }
-            else if (Content == matrics[0, 2])
-            {
-                matrics[0, 2] = player;
-            }
-            else if (Content == matrics[1,0])
-            {
-                matrics[1, 0] = player;
-            }
-            else if (Content == matrics[1,1])
-            {
-                matrics[1, 1] = player;
-            }
-            else if (Content == matrics[1,2])
-            {
-                matrics[1, 2] = player;
-            }
-            else if (Content == matrics[2,0])
-            {
-                matrics[2, 0] = player;
-            }
-            else if (Content == matrics[2,1])
-            {
-                matrics[2, 1] = player;
-            }
-            else if (Content == matrics[2, 2])
-            {
-                matrics[2,2] = player;
-            }
+            SetField();
 
 
         }
-        public static void CheckOut(string palyer)
+        public static void SetField()
         {
-            if (matrics[0, 0] == matrics[0, 1] && matrics[0, 1]== matrics[0, 2] ||
-                matrics[1, 0] == matrics[1, 1] && matrics[1, 0] == matrics[1, 2] ||
-                matrics[2, 0] == matrics[2, 1] && matrics[2, 0] == matrics[2, 2] ||
-                matrics[0, 0] == matrics[1, 0] && matrics[0, 0] == matrics[2, 0] ||
-                matrics[0, 1] == matrics[1, 1] && matrics[0, 1] == matrics[2, 1] ||
-                matrics[0, 2] == matrics[1, 2] && matrics[0, 2] == matrics[2, 2] ||
-                matrics[0, 0] == matrics[1, 1] && matrics[0, 0] == matrics[2, 2] ||
-                matrics[0, 2] == matrics[1, 1] && matrics[0,2] == matrics[2, 1]
+            
+            Console.WriteLine("     |     |      ");
+            //TODO replace numbers with variebles
+            Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            //TODO replace numbers with variebles
+            Console.WriteLine("  {0}  |  {1}  |  {2}", 4, 5, 6);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            //TODO replace numbers with variebles
+            Console.WriteLine("  {0}  |  {1}  |  {2}", 7, 8, 9);
+            Console.WriteLine("     |     |     ");
+            
+        }
 
-                 )
-            {
-                Console.WriteLine("you are win {0}", palyer);
-            }
-            else
-            {
 
-            }
-    }
+
+
     }
 
 }
