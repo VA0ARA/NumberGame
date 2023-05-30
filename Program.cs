9ba1bc46fbe350e7d85f19c2bb49cf3139e83bcb
vahid ara <vahidaara@gmail.com>
2023-05-30 08:42:35 +0430

    write EnterOOorX method

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ namespace TicTacToeGame
 {
     class Program
     {
+        //the playfield
+        static char[,] playField =
+            {
+                {'1','2','3'},  //Row 0
+                {'4','5','6'},  //Row 1
+                {'7','8','9'}   //Row 2
+            };
 
         static void Main(string[] args)
         {
@@ -13,20 +20,20 @@ namespace TicTacToeGame
         }
         public static void SetField()
         {
-            
+
             Console.WriteLine("     |     |      ");
             //TODO replace numbers with variebles
-            Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[0, 0], playField[0, 1], playField[0, 2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
             //TODO replace numbers with variebles
-            Console.WriteLine("  {0}  |  {1}  |  {2}", 4, 5, 6);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[1, 0], playField[1, 1], playField[1, 2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
             //TODO replace numbers with variebles
-            Console.WriteLine("  {0}  |  {1}  |  {2}", 7, 8, 9);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[2, 0], playField[2, 1], playField[2, 2]);
             Console.WriteLine("     |     |     ");
-            
+
         }
 
 
